5c1cdc06da3f830ce64051e7a2f5696553111168
Vi Pham <pham0101@algonquinlive.com>
2019-08-19 21:29:42 -0400

    fixed backdrop

--- src/components/UI/Backdrop/Backdrop.js
+++ src/components/UI/Backdrop/Backdrop.js
@@ -2,6 +2,10 @@ import React from "react";
 import css from "./Backdrop.module.css";
 
 const backdrop = props =>
-  props.show ? <div className={css.Backdrop} onClick={props.clicked} /> : null;
+  props.show ? (
+    <div className={css.Backdrop} onClick={props.clicked}>
+      {props.children}
+    </div>
+  ) : null;
 
 export default backdrop;
