af64b0e94621c04a4cf7dca00f733f25be020228
Vi Pham <pham0101@algonquinlive.com>
2019-08-23 10:40:46 -0400

    added toolbar

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,10 +1,11 @@
 import React from "react";
 import Ax from "../../hoc/Ax";
 import Classes from "./Layout.module.css";
+import Toolbar from "../Navigation/Toolbar/Toolbar";
 
 const layout = props => (
   <Ax>
-    <div>Toolbar, SideDrawer, Backdrop</div>
+    <Toolbar />
     <main className={Classes.Content}>{props.children}</main>
   </Ax>
 );
